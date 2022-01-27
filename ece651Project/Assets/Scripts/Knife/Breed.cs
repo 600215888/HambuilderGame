ae0337d6da9cea5e69bc2942e752f5a3c12091a9
Elikem Buertey <ebuertey@uwaterloo.ca>
2022-01-26 21:58:27 -0500

    Started working on knife. Added breed, enemy etc

---
+++ ece651Project/Assets/Scripts/Knife/Breed.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface Breed
+{
+    // Start is called before the first frame update
+    void move(GameObject gameobj);
+}
---
+++ ece651Project/Assets/Scripts/Knife/Left_Right.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Left_Right : Breed
+{
+    // Start is called before the first frame update
+    public void move(GameObject gameobj)
+    {
+        throw new System.NotImplementedException();
+    }
+}
