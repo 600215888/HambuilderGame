09ad69df6e7f1423d7f57b6d985cbf2de7d9c652
Elikem Buertey <ebuertey@uwaterloo.ca>
2022-01-26 19:52:07 -0500

    add rotation to knife

--- ece651Project/Assets/knife.cs
+++ ece651Project/Assets/knife.cs
@@ -8,12 +8,15 @@ public class knife : MonoBehaviour
     public float movementSpeed = 3.0f;
     private GameObject knifeobj;
     Vector2 movement = new Vector2();
+    public int rot_angle = -30;
+    public bool go_back = true;
 
     Rigidbody2D rb2D;
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
         knifeobj = GameObject.Find("KnifeObject");
+        knifeobj.transform.rotation = Quaternion.Euler(Vector3.forward * (rot_angle));
     }
 
     private void FixedUpdate()
@@ -27,12 +30,35 @@ public class knife : MonoBehaviour
         // 8
         rb2D.velocity = movement * movementSpeed;
 
+        if (rot_angle > 30)
+        {
+            rot_angle -= 60;
+            go_back = true;
+        }
+
+        if (go_back == true)
+        {
+            rot_angle -= 3;
+        }
+
+        if (rot_angle < -30)
+        {
+            go_back = false;
+        }
+
+        if (go_back == false)
+        {
+            rot_angle += 1;
+        }
+        
+        knifeobj.transform.rotation = Quaternion.Euler(Vector3.forward * (rot_angle));
+
         //movement.y += movementSpeed;
         //movement.Normalize();
         //rb2D.velocity = movement * movementSpeed;
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerExit2D(Collider2D collision)
     {
         knifeobj.transform.position = new Vector2(0,4);
     }
