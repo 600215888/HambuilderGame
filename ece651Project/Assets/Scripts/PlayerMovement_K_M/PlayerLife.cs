edf7b30d7f374fa28d4fa061167d983f39d7127a
Platelet-0v0 <FortressPachina@gmail.com>
2022-02-01 19:31:43 -0500

    Level0
    complete prefabs, change/add scripts for platform, knife reset, player death

--- ece651Project/Assets/Scripts/PlayerMovement_K_M/PlayerLife.cs
+++ ece651Project/Assets/Scripts/PlayerMovement_K_M/PlayerLife.cs
@@ -30,6 +30,21 @@ public class PlayerLife : MonoBehaviour
         }
     }
 
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("MapBorder"))
+        {
+            Die();
+        }
+
+        if (collision.gameObject.CompareTag("SwayDown") || collision.gameObject.CompareTag("LeftRight"))
+        {
+            Die();
+        }
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Trap"))
