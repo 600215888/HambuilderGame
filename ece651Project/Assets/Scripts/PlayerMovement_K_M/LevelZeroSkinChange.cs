4a9d14449afb45ebb8a5c2449553de1317949a3e
Tingcen <t33dong@uwaterloo.ca>
2022-02-07 17:00:27 -0500

    finish skin change

--- ece651Project/Assets/Scripts/PlayerMovement_K_M/LevelZeroSkinChange.cs
+++ ece651Project/Assets/Scripts/PlayerMovement_K_M/LevelZeroSkinChange.cs
@@ -6,9 +6,16 @@ public class LevelZeroSkinChange : MonoBehaviour
 {
     [SerializeField] private AnimatorOverrideController levelfive;
     [SerializeField] private AnimatorOverrideController levelone;
+    [SerializeField] private AnimatorOverrideController leveltwo;
+    [SerializeField] private AnimatorOverrideController levelthree;
+    [SerializeField] private AnimatorOverrideController levelfour;
+
+    private AnimatorOverrideController[] controllerArray;
+
     void Start()
     {
-        
+        //controllerArray[0] = levelone;
+        controllerArray = new AnimatorOverrideController[] { levelone, leveltwo, levelthree, levelfour, levelfive };
     }
 
     // Update is called once per frame
@@ -18,11 +25,37 @@ public class LevelZeroSkinChange : MonoBehaviour
         {
             ChangeCharSkin(1);
         }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            ChangeCharSkin(2);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            ChangeCharSkin(3);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            ChangeCharSkin(4);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            ChangeCharSkin(5);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            ChangeCharSkin(0);
+        }
     }
 
     private void ChangeCharSkin(int skinIndex)
     {
-        //do a case switch for all skins?
-        GetComponent<Animator>().runtimeAnimatorController = levelone as RuntimeAnimatorController;
+        if (skinIndex == 0)
+        {
+            GetComponent<Animator>().runtimeAnimatorController = Resources.Load("Animation/Hamburger") as RuntimeAnimatorController;
+        }
+        else
+        {
+            GetComponent<Animator>().runtimeAnimatorController = controllerArray[skinIndex-1] as RuntimeAnimatorController;
+        }
     }
 }
