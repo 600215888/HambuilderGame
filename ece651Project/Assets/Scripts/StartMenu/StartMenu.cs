62139a251b60a67aa60e7559821d0853df72aa87
BRIAN LUONG <brian.luong@hotmail.ca>
2022-02-10 17:05:13 -0500

    added background music and button sounds

--- ece651Project/Assets/Scripts/StartMenu/StartMenu.cs
+++ ece651Project/Assets/Scripts/StartMenu/StartMenu.cs
@@ -7,7 +7,7 @@ public class StartMenu : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("Level0");
     }
     public void LoadMainMenu()
     {
