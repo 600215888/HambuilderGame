20bc0c16d69bb5e5574b81ba7d7edde19bb1f079
Tingcen <t33dong@uwaterloo.ca>
2022-02-09 22:09:19 -0500

    update audios to prefabs

--- ece651Project/Assets/Scripts/Knife/Knife.cs
+++ ece651Project/Assets/Scripts/Knife/Knife.cs
@@ -12,12 +12,14 @@ public class Knife : MonoBehaviour
     public int rot_angle = 0;
     public bool go_back = true;
     private Vector3 orig_position;
+    [SerializeField] private AudioSource soundeffect;
 
     Rigidbody2D rb2D;
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
         knifeobj = this.gameObject;
+        soundeffect.Play();
         knifeobj.transform.rotation = Quaternion.Euler(Vector3.forward * (rot_angle));
         orig_position = knifeobj.transform.position;
     }
@@ -61,6 +63,7 @@ public class Knife : MonoBehaviour
         if (collision.gameObject.tag == "Ground")
         {
             knifeobj.transform.position = orig_position;
+            soundeffect.Play();
         }
     }
 
