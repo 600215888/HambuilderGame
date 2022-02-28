cdeee21ea524d28a9cdb8ec19dae706ec4c1acdf
Elikem Buertey <ebuertey@uwaterloo.ca>
2022-02-27 22:52:02 -0500

    Added build script

---
+++ ece651Project/Assets/Scripts/Editor/BuildScript.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+public class BuildScript : MonoBehaviour
+{
+    public static void PerformBuild()
+    {
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[] { "Assets/Scenes/MainMenuSingleScene.unity", "Assets/Scenes/Level0.unity" };
+        buildPlayerOptions.locationPathName = "./builds/hamburgerx64.exe";
+        buildPlayerOptions.target = BuildTarget.StandaloneWindows64;
+        buildPlayerOptions.options = BuildOptions.Development;
+
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        BuildSummary summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+        }
+
+        if (summary.result == BuildResult.Failed)
+        {
+            Debug.Log("Build failed");
+        }
+    }
+}
+
