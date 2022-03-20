6143652c48239431c0db7c8049e120a099c61e8a
Tingcen Dong <t33dong@uwaterloo.ca>
2022-03-20 01:33:18 +0000

    all level final sequence and transitions

--- ece651Project/Assets/Scripts/FinalSequence/NextLevel.cs
+++ ece651Project/Assets/Scripts/FinalSequence/NextLevel.cs
@@ -5,6 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class NextLevel : MonoBehaviour
 {
+    [SerializeField] private RuntimeAnimatorController lone;
+    [SerializeField] private AnimatorOverrideController ltwo;
+    [SerializeField] private AnimatorOverrideController lthree;
+    [SerializeField] private AnimatorOverrideController lfour;
+    [SerializeField] private AnimatorOverrideController lfive;
+
+    private AnimatorOverrideController[] controllerArray;
+
+    [SerializeField] private new GameObject light;
+    [SerializeField] private GameObject player;
     private AudioSource goalReachSound;
     private bool activate;
     
@@ -12,26 +22,62 @@ public class NextLevel : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
+        SetToppingController();
+        int levelIndex = SceneManager.GetActiveScene().buildIndex;
+        if (levelIndex == 2)
+        {
+            GetComponent<Animator>().runtimeAnimatorController = lone as RuntimeAnimatorController;
+        }
+        else
+        {
+            GetComponent<Animator>().runtimeAnimatorController = controllerArray[levelIndex - 3]  as RuntimeAnimatorController;
+        }
         goalReachSound = GetComponent<AudioSource>();
         activate = false;
+        Debug.Log(GetComponent<Animator>().runtimeAnimatorController.name);
     }
+
+
+    public void SetToppingController()
+    {
+        controllerArray = new AnimatorOverrideController[] { ltwo, lthree, lfour, lfive };
+    }
+
+    
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player") && !activate)
         {
             activate = true;
+            Destroy(GetComponent<SpriteRenderer>());
+            Destroy(light);
             goalReachSound.Play();
+            StartCoroutine(SkinChangeAnim());
             StartCoroutine(GoToNextLevel());
         }
     }
 
 
+    IEnumerator SkinChangeAnim()
+    {
+
+
+        //change skin animation
+        player.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+        player.GetComponent<PlayerMovement>().isStatic = true;
+        player.GetComponent<Animator>().Play("Ham_Change");
+        yield return new WaitForSeconds(5);
+
+    }
+
+
     IEnumerator GoToNextLevel()
     {
-        
+
         yield return new WaitForSeconds(2);
-        if (SceneManager.GetActiveScene().buildIndex == 5)
+        if (SceneManager.GetActiveScene().buildIndex == 6)
         {
             SceneManager.LoadScene(0);
         }
