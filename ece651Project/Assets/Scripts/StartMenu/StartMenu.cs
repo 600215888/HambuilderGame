3ebbbed2c0fef27f44ad713184d9ac65d3fb5923
Tingcen <t33dong@uwaterloo.ca>
2022-03-23 14:39:37 -0400

    update on load level select cs

--- ece651Project/Assets/Scripts/StartMenu/StartMenu.cs
+++ ece651Project/Assets/Scripts/StartMenu/StartMenu.cs
@@ -7,7 +7,7 @@ public class StartMenu : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene("Level 1");
+        SceneManager.LoadScene(2);
     }
     public void LoadMainMenu()
     {
@@ -20,23 +20,23 @@ public class StartMenu : MonoBehaviour
     }
     public void Level1()
     {
-        SceneManager.LoadScene(1); //assuming levels are in build settings as lvl1=1, lvl2=2...
+        SceneManager.LoadScene(2); //assuming levels are in build settings as lvl1=1, lvl2=2...
     }
     public void Level2()
     {
-        SceneManager.LoadScene(2);
+        SceneManager.LoadScene(3);
     }
     public void Level3()
     {
-        SceneManager.LoadScene(3);
+        SceneManager.LoadScene(4);
     }
     public void Level4()
     {
-        SceneManager.LoadScene(4);
+        SceneManager.LoadScene(5);
     }
     public void Level5()
     {
-        SceneManager.LoadScene(5);
+        SceneManager.LoadScene(6);
     }
     public void QuitGame()
     {
